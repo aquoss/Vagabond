f7ca4076dcef1c8d812e12cbb3afd3e8f72d865f
aapiane09 <aapiane09@gmail.com>
2017-01-31 17:51:33 -0800

    Sprint 1.1 - Adds splash page with title and signup/login functionality

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user_params = params.require(:user).permit(:email, :password)
+    # confirm that email/password combination is correct
+    @user = User.confirm(user_params)
+    if @user
+      login(@user)
+      flash[:notice] = "Successfully logged in."
+      redirect_to @user
+    else
+      flash[:error] = "Incorrect email or password."
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    logout(@user)
+    flash[:notice] = "Successfully logged out."
+    redirect_to root_path
+  end
+  
+end
---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,12 @@
+module SessionsHelper
+
+  def login(user)
+    session[:user_id] = user.id
+    @current_user = user
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+end
