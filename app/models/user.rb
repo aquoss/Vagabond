d5cb31a621d0c136318e464220e6ad8637c2392f
aquoss <amberquoss@gmail.com>
2017-02-01 19:30:02 -0800

    connects carrierwave profile photo files

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
 
+  mount_uploader :photo, PhotoUploader
+
   def self.confirm(params)
     @user = User.find_by({email: params[:email]})
     @user ? @user.authenticate(params[:password]) : false
