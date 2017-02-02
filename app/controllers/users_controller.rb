3e4256a6184f086711c991313d565034121711f8
aquoss <amberquoss@gmail.com>
2017-02-02 12:16:29 -0800

    adds validation for email and posts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,9 +16,10 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
-      redirect_to '/'
+      redirect_to root
     else
-      redirect_to '/signup'
+      flash[:error] = user.errors.full_messages.join(", ")
+      redirect_to new_user_path
     end
   end
 
