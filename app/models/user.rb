652e88b20d63e0caf15146a29993ba410cc37100
aquoss <amberquoss@gmail.com>
2017-02-03 12:38:08 -0800

    validates presence of form fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   mount_uploader :photo, PhotoUploader
 
   validates :email, uniqueness: {message: " is already taken! Log into your existing account or choose a different address."}
+  validates_presence_of :email, :password, :first_name, :last_name
 
   def self.confirm(params)
     @user = User.find_by({email: params[:email]})
