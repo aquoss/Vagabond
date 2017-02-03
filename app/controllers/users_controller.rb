90faea0127079475409a457a0a808490d318cfd3
aquoss <amberquoss@gmail.com>
2017-02-03 11:52:25 -0800

    fixes broken root path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
-      redirect_to root
+      redirect_to root_path
     else
       flash[:error] = user.errors.full_messages.join(", ")
       redirect_to new_user_path
