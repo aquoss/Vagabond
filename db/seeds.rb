11992c89b3477b651b5394380e93872b99761bd8
mblair415 <mblair415@gmail.com>
2017-02-04 20:14:32 -0800

    gifs added for each city. in seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,28 +27,36 @@ user = User.create(user_data)
 
 city_data = [{
     city_name: 'San Francisco',
-    photo: "http://www.sftravel.com/sites/sftraveldev.prod.acquia-sites.com/files/SanFrancisco_0.jpg"
+    photo: "http://www.sftravel.com/sites/sftraveldev.prod.acquia-sites.com/files/SanFrancisco_0.jpg",
+    gif: "https://media.giphy.com/media/josB0ZKSutNgA/giphy.gif"
   }, {
     city_name: "London",
-    photo: "https://media.timeout.com/images/100644443/image.jpg"
+    photo: "https://media.timeout.com/images/100644443/image.jpg",
+    gif: "https://media.giphy.com/media/2Ozjbk786Umdy/giphy.gif"
   }, {
     city_name: "Gibraltar",
-    photo: "http://www.officialrocktours.com/images/mob/gibraltar-aerial.jpg"
+    photo: "http://www.officialrocktours.com/images/mob/gibraltar-aerial.jpg",
+    gif: "https://media.giphy.com/media/12vbDdhxvStvWw/giphy.gif"
   }, {
     city_name: "Tokyo",
-    photo: "http://vacationadvice101.com/wp-content/uploads/2013/04/Tokyo-Tower-and-Tokyo-Skyline-at-night.jpg"
+    photo: "http://vacationadvice101.com/wp-content/uploads/2013/04/Tokyo-Tower-and-Tokyo-Skyline-at-night.jpg",
+    gif: "https://media.giphy.com/media/xTiTndRhkTeDSUEgak/giphy.gif"
   }, {
     city_name: "Beijing",
-    photo: "https://www.sarahlawrence.edu/media/study-abroad-and-exchange/beijing/beijing_main_shutterstock_111796310.jpg"
+    photo: "https://www.sarahlawrence.edu/media/study-abroad-and-exchange/beijing/beijing_main_shutterstock_111796310.jpg",
+    gif: "https://media.giphy.com/media/Cz23XAf6TNJCM/source.gif"
   }, {
     city_name: "Sydney",
-    photo: "http://www.allianceabroad.com/wp-content/uploads/sydney_harbour.jpg"
+    photo: "http://www.allianceabroad.com/wp-content/uploads/sydney_harbour.jpg",
+    gif: "https://media.giphy.com/media/aUmSzhzcR2p6E/giphy.gif"
   }, {
     city_name: "Paris",
-    photo: "http://images.huffingtonpost.com/2015-11-19-1447968585-1661590-6672156239_89c77d53d8_o.jpg"
+    photo: "http://images.huffingtonpost.com/2015-11-19-1447968585-1661590-6672156239_89c77d53d8_o.jpg",
+    gif: "https://media.giphy.com/media/vXEeRBP3QeJ2w/giphy.gif"
   }, {
-    city_name: "Reykavik",
-    photo: "http://blog.radissonblu.com/wp-content/uploads/2015/04/Reykjavik-city.jpg"
+    city_name: "Reykjavik",
+    photo: "http://blog.radissonblu.com/wp-content/uploads/2015/04/Reykjavik-city.jpg",
+    gif: "https://media.giphy.com/media/4EhFiUaNYyZZS/giphy.gif"
   }
 ]
 city = City.create(city_data)
